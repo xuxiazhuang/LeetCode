bb41a4275a3a708d08eb81ac39f70d9fa06553f8
xuxiazhuang <42253430+xuxiazhuang@users.noreply.github.com>
2018-12-12 23:51:25 -0800

    add 811

---
+++ python_leetcode/_811.py
@@ -0,0 +1,107 @@
+A website domain like "discuss.leetcode.com" consists of various subdomains. At the top level, we have "com", at the next level, we have "leetcode.com", and at the lowest level, "discuss.leetcode.com". When we visit a domain like "discuss.leetcode.com", we will also visit the parent domains "leetcode.com" and "com" implicitly.
+
+Now, call a "count-paired domain" to be a count (representing the number of visits this domain received), followed by a space, followed by the address. An example of a count-paired domain might be "9001 discuss.leetcode.com".
+
+We are given a list cpdomains of count-paired domains. We would like a list of count-paired domains, (in the same format as the input, and in any order), that explicitly counts the number of visits to each subdomain.
+
+Example 1:
+Input: 
+["9001 discuss.leetcode.com"]
+Output: 
+["9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com"]
+Explanation: 
+We only have one website domain: "discuss.leetcode.com". As discussed above, the subdomain "leetcode.com" and "com" will also be visited. So they will all be visited 9001 times.
+
+Example 2:
+Input: 
+["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
+Output: 
+["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"]
+Explanation: 
+We will visit "google.mail.com" 900 times, "yahoo.com" 50 times, "intel.mail.com" once and "wiki.org" 5 times. For the subdomains, we will visit "mail.com" 900 + 1 = 901 times, "com" 900 + 50 + 1 = 951 times, and "org" 5 times.
+
+
+solution:
+class Solution:
+    def subdomainVisits(self, cpdomains):
+        """
+        :type cpdomains: List[str]
+        :rtype: List[str]
+        """
+        ans = collections.Counter()
+        for domain in cpdomains:
+            count, domain = domain.split()
+            count = int(count)
+            frags = domain.split('.')
+            for i in range(len(frags)):
+                ans[".".join(frags[i:])] += count
+
+        return ["{} {}".format(ct, dom) for dom, ct in ans.items()]
+        
+        
+        
+  explanation(example):
+A=["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
+import collections
+ans = collections.Counter()
+for domain in A:
+	count,domain = domain.split()
+	count = int(count)
+	frags = domain.split('.')
+	for i in range(len(frags)):
+		# print(".".join(frags[i:]))
+		ans[".".join(frags[i:])] = count + ans[".".join(frags[i:])]
+print(ans.items())
+print(["{} {}".format(ct, dom) for dom, ct in ans.items()])
+
+#####
+collections.Counter() 
+A counter is a container that stores elements as dictionary keys, and their counts are stored as dictionary values.
+>>> from collections import Counter
+>>> 
+>>> myList = [1,1,2,3,4,5,3,2,3,4,2,1,2,3]
+>>> print Counter(myList)
+Counter({2: 4, 3: 4, 1: 3, 4: 2, 5: 1})
+>>>
+>>> print Counter(myList).items()
+[(1, 3), (2, 4), (3, 4), (4, 2), (5, 1)]
+>>> 
+>>> print Counter(myList).keys()
+[1, 2, 3, 4, 5]
+>>> 
+>>> print Counter(myList).values()
+[3, 4, 4, 2, 1]
+
+
+###
+str.format() is one of the string formatting methods in Python3, which allows multiple substitutions and value formatting. 
+This method lets us concatenate elements within a string through positional formatting.
+
+print ("This is {} {} {} {}"
+       .format("one", "two", "three", "four"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
